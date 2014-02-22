5256e2b24610c744823e0030317f1f068ee4194f
xhzengAIB <xhzengAIB@gmail.com>
2014-02-22 20:24:22 +0000

    Add XHMusicCell class

---
+++ Source/XHMusicCell.m
@@ -0,0 +1,29 @@
+//
+//  XHMusicCell.m
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "XHMusicCell.h"
+
+@implementation XHMusicCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
