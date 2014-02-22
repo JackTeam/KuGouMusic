16d438e3f6cc1fc8112eede8cb095936dc6c75bb
xhzengAIB <xhzengAIB@gmail.com>
2014-02-22 20:49:04 +0000

    implementation XHMusicCell logic

--- Source/XHMusicCell.m
+++ Source/XHMusicCell.m
@@ -8,6 +8,14 @@
 
 #import "XHMusicCell.h"
 
+@interface XHMusicCell ()
+
+@property (nonatomic, strong) UIImageView *palugImageView;
+@property (nonatomic, strong) UILabel *songNameLabel;
+@property (nonatomic, strong) UIImageView *cheakImageView;
+
+@end
+
 @implementation XHMusicCell
 
 #pragma mark - Propertys
@@ -21,11 +29,26 @@
 
 #pragma mark - life cycle
 
+- (void)_setup {
+    _palugImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    _palugImageView.backgroundColor = [UIColor grayColor];
+    
+    _cheakImageView = [[UIImageView alloc] initWithFrame:CGRectMake(CGRectGetWidth([[UIScreen mainScreen] bounds]) - 30, 0, 30, 30)];
+    _cheakImageView.backgroundColor = [UIColor redColor];
+    
+    _songNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMaxX(_palugImageView.frame), 0, CGRectGetWidth([[UIScreen mainScreen] bounds]) - CGRectGetWidth(_palugImageView.frame) - CGRectGetWidth(_cheakImageView.frame), 30)];
+    
+    [self.contentView addSubview:self.palugImageView];
+    [self.contentView addSubview:self.cheakImageView];
+    [self.contentView addSubview:self.songNameLabel];
+}
+
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
+        [self _setup];
     }
     return self;
 }
