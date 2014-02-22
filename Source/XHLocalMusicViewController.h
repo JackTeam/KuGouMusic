7c5ba6eb297d8b71c08a94c7770cf143f921441c
xhzengAIB <xhzengAIB@gmail.com>
2014-02-22 20:22:28 +0000

    Add XHLocalMusicViewController class

---
+++ Source/XHLocalMusicViewController.h
@@ -0,0 +1,13 @@
+//
+//  XHLocalMusicViewController.h
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XHLocalMusicViewController : UIViewController
+
+@end
---
+++ Source/XHLocalMusicViewController.m
@@ -0,0 +1,59 @@
+//
+//  XHLocalMusicViewController.m
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "XHLocalMusicViewController.h"
+
+@interface XHLocalMusicViewController ()
+
+@end
+
+@implementation XHLocalMusicViewController
+
+#pragma mark - Life cycle
+
+- (void)_setup {
+    
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        [self _setup];
+    }
+    return self;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
