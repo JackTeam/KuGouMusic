1a0af7d4e627c099373003610ed266d2d4c9a7c2
xhzengAIB <xhzengAIB@gmail.com>
2014-02-22 20:15:30 +0000

    Add project

---
+++ KugouMusic/KugouMusic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KugouMusic/KugouMusicTests/KugouMusicTests.m
@@ -0,0 +1,34 @@
+//
+//  KugouMusicTests.m
+//  KugouMusicTests
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KugouMusicTests : XCTestCase
+
+@end
+
+@implementation KugouMusicTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
