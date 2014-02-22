da778a27cb12cd7d0580ca8b9f010ace0053b708
xhzengAIB <xhzengAIB@gmail.com>
2014-02-22 20:25:20 +0000

    Add XHMusic class

---
+++ Source/XHMusic.h
@@ -0,0 +1,13 @@
+//
+//  XHMusic.h
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XHMusic : NSObject
+
+@end
---
+++ Source/XHMusic.m
@@ -0,0 +1,13 @@
+//
+//  XHMusic.m
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "XHMusic.h"
+
+@implementation XHMusic
+
+@end
