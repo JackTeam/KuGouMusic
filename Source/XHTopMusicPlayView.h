deac493b1354900289158aa05b76fe8e748de6ab
xhzengAIB <xhzengAIB@gmail.com>
2014-02-22 20:27:23 +0000

    Add XHTopMusicPlayView class to singleton

---
+++ Source/XHTopMusicPlayView.h
@@ -0,0 +1,13 @@
+//
+//  XHTopMusicPlayView.h
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XHTopMusicPlayView : UIView
+
+@end
---
+++ Source/XHTopMusicPlayView.m
@@ -0,0 +1,31 @@
+//
+//  XHTopMusicPlayView.m
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "XHTopMusicPlayView.h"
+
+@implementation XHTopMusicPlayView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
