2247e25787d8e1d407f00cacfa055af514a5ce44
xhzengAIB <xhzengAIB@gmail.com>
2014-02-22 20:33:27 +0000

    Add XHMusic category provider due to data manager

---
+++ Source/XHMusic+Provider.h
@@ -0,0 +1,13 @@
+//
+//  XHMusic+Provider.h
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "XHMusic.h"
+
+@interface XHMusic (Provider)
+
+@end
---
+++ Source/XHMusic+Provider.m
@@ -0,0 +1,13 @@
+//
+//  XHMusic+Provider.m
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-22.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "XHMusic+Provider.h"
+
+@implementation XHMusic (Provider)
+
+@end
