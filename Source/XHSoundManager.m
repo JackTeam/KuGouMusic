b7ad1b01878806c133fd68baa28d67d977033bc0
xhzengAIB <xhzengAIB@gmail.com>
2014-02-23 00:16:29 +0000

    Add XHSoundManager class

---
+++ Source/XHSoundManager.m
@@ -0,0 +1,13 @@
+//
+//  XHSoundManager.m
+//  KugouMusic
+//
+//  Created by 曾 宪华 on 14-2-23.
+//  Copyright (c) 2014年 HUAJIE. All rights reserved.
+//
+
+#import "XHSoundManager.h"
+
+@implementation XHSoundManager
+
+@end
